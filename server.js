a11987df1ff9cb53c1897a41677c9f2fbaec288b
Govind Rai <raigovind93@gmail.com>
2017-05-16 11:49:52 -0700

    Abstract port selection for heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getCurrentYear', () => {
@@ -58,6 +59,6 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log("Server is up and running on port 3000")
-})
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});
